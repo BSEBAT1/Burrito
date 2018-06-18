5dc1590cb53ee118d4ae850572f4cd37f91eccc8
Berkay <Berkay_Sebat@yahoo.com>
2018-06-17 20:59:18 -0400

    Initial Commit

---
+++ NoStoryBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoStoryBoard
+//
+//  Created by macbook pro on 6/17/18.
+//  Copyright © 2018 Sebat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
